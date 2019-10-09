44b18f92a9e10c18022b904d775210a7abf7113d
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-09 16:09:14 -0400

    Added Request Queue

--- app/src/main/java/com/remijonathan/parsing/MainActivity.java
+++ app/src/main/java/com/remijonathan/parsing/MainActivity.java
@@ -3,25 +3,31 @@ package com.remijonathan.parsing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestQueue = Volley.newRequestQueue(this);
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://jsonplaceholder.typicode.com/todos/1", null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-
+                Log.d("json","onResponse: "+response);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -29,5 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        requestQueue.add(jsonObjectRequest);
     }
 }
